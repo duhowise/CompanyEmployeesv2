d6401bd848302b91c09c0e6723cca289a3ea7a2c
Wise Duho <duhowise@gmail.com>
2022-02-26 15:39:35 +0000

    remoted companny dto constructor

--- Entities/DataTransferObjects/CompanyDto.cs
+++ Entities/DataTransferObjects/CompanyDto.cs
@@ -3,16 +3,7 @@
 public class CompanyDto
 {
    
-
-    public CompanyDto(Guid id, string name, string fullAddress)
-    {
-        Id = id;
-        Name = name;
-        FullAddress = fullAddress;
-
-    }
-
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string FullAddress { get; set; }
+    public string? Name { get; set; }
+    public string? FullAddress { get; set; }
 }
